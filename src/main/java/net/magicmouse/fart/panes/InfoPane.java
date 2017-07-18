906034440ba3a0c9a27f49ae4f4c2e6beceb7704
PN! <peter.nielsen@arbetsformedlingen.se>
2017-07-18 10:34:15 +0200

    .

--- src/main/java/net/magicmouse/fart/panes/InfoPane.java
+++ src/main/java/net/magicmouse/fart/panes/InfoPane.java
@@ -1,6 +1,7 @@
 package net.magicmouse.fart.panes;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by niepe on 2017-07-17.
@@ -8,12 +9,19 @@ import javax.swing.*;
 public class InfoPane extends JPanel {
 
     JLabel jLabel;
+    JButton jButton;
 
     public InfoPane() {
 
+        this.setLayout(new GridLayout(0,1));
+        this.setSize(200, 600);
+
+
         jLabel = new JLabel("Hello World!");
+        jButton = new JButton("Mountain this square");
 
         this.add(jLabel);
+        this.add(jButton);
 
         this.setVisible(true);
 
